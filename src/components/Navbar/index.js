171830a1d753fc5d4a2c2ab61705b3a4a1955962
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-01-02 11:52:49 +0530

    implementing common dialog component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,28 @@
 "use client";
 
+import { GlobalContext } from "@/context";
 import { adminNavOptions, navOptions, styles } from "@/utils";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import CommonModal from "../CommonModal";
 
 const isAdminView = false;
 const isAuthUser = true;
 const user = {
   role: "admin",
 };
-function NavItems() {
+function NavItems({ isModalView = false }) {
   return (
     <div
-      className="item-center justify-between w-full md:w-auto"
+      className={`items-center justify-between w-full md:flex md:w-auto ${
+        isModalView ? "" : "hidden"
+      }`}
       id="nav-items"
     >
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white">
+      <ul
+        className={`flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white ${
+          isModalView ? "border-none" : "border border-gray-100"
+        }`}
+      >
         {isAdminView
           ? adminNavOptions.map((item) => (
               <li
@@ -38,10 +46,11 @@ function NavItems() {
 }
 
 export default function Navbar() {
+  const { showNavModal, setShowNavModal } = useContext(GlobalContext);
   return (
     <>
       <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
-        <div className="max-w-screen-xl flex flex-wrap item-center justify-between mx-auto p-4">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="flex items-center cursor-pointer">
             <span className="self-center text-2xl font-semibold whitespace-nowrap">
               Ecommerce
@@ -61,13 +70,18 @@ export default function Navbar() {
                 <button className={styles.button}>Admin View</button>
               )
             ) : null}
-            {isAuthUser ? <button className={styles.button}>Logout</button> : <button className={styles.button}>Login</button>}
+            {isAuthUser ? (
+              <button className={styles.button}>Logout</button>
+            ) : (
+              <button className={styles.button}>Login</button>
+            )}
             <button
               data-collapse-toggle="navbar-sticky"
               type="button"
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-sticky"
               aria-expanded="false"
+              onClick={() => setShowNavModal(true)}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -88,6 +102,12 @@ export default function Navbar() {
           <NavItems />
         </div>
       </nav>
+      <CommonModal
+        showModalTitle={false}
+        mainContent={<NavItems isModalView={true} />}
+        show={showNavModal}
+        setShow={setShowNavModal}
+      />
     </>
   );
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -41,7 +41,7 @@ export const adminNavOptions = [
 
 export const styles = {
   button:
-    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white",
+    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white",
 };
 
 export const registrationFormControls = [
