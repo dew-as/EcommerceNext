a10a567ebcf8a2518fc685e082e1ca3c233d92ac
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-01-23 16:46:29 +0530

    added admin view get all orders of all users and update api for order confirmation

--- src/services/order/index.js
+++ src/services/order/index.js
@@ -54,3 +54,41 @@ export const getOrderDetails = async (id) => {
     console.log(error);
   }
 };
+
+export const getAllOrdersForAllUser = async () => {
+  // this is basically user id for get all orders
+  try {
+    const res = await fetch("/api/admin/order/get-all-orders", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    });
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateStatusOfOrder = async (formData) => {
+  // this is basically user id for get all orders
+  try {
+    const res = await fetch("/api/admin/order/update-order", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+      body: JSON.stringify(formData)
+    });
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
